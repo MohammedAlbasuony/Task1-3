b92ea34621012242edb900d293b20b2e99e2b677
Mohammed Albasuony <Mido.beso.hosny@gmail.com>
2024-07-28 18:38:22 +0300

    Add project files.

---
+++ Task 1-3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_1_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter Rectangle width :  ");
+            int Width = int.Parse(Console.ReadLine());
+            
+            Console.Write("Enter Rectangle hight:  ");
+            int Hight = int.Parse(Console.ReadLine());
+
+            if (Width == Hight) 
+            {
+                Console.Write("The information you inserted is for a Square ");
+            }
+            else
+            {
+                Console.Write("The information you inserted is for a Rectangle");
+            }
+           
+        }
+    }
+}
